45772e2f06fbeec616e904945e5dd82a8e2e4cb8
kavinkumar06 <kavinm09@gmail.com>
2017-07-13 14:14:05 +0530

    Create Graetest.java

---
+++ Graetest.java
@@ -0,0 +1,27 @@
+# Greatest
+import java.io.*;
+import java.util.*;
+public class Greatest
+{
+  public static void main(String args[])
+  {
+   int a,b,c;
+   Scanner sc=new Scanner(System.in);
+   System.out.println(Enter any three numbers:");
+   a=sc.nextInt();
+   b=sc.nextInt();
+   c=sc.nextInt();
+   if(a>=b && a>=c)
+   { 
+     System.out.println("Largest number:"+a);
+   }
+   if(b>=a && b>=c)
+   {
+     System.out.println("Largest number:"+b);
+   }
+   if(c>=a && c>=b)
+   {
+     System.out.println("Largest number:"+c);
+     }
+    }
+   } 
